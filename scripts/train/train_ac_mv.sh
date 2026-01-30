e0652635f4935a6810ec956091be950f3a41ac3f
Chen Xin <chen.xin@agile-robots.com>
2026-01-30 18:54:55 +0100

    move to subfolder

--- scripts/train/train_ac_mv.sh
+++ scripts/train/train_ac_mv.sh
@@ -8,13 +8,13 @@ set -e
 export IMAGINAIRE_OUTPUT_ROOT=/raid/chen.xin/repo/cosmos-predict2.5/imaginaire-output
 
 # Set visible GPU(s)
-export CUDA_VISIBLE_DEVICES=3,4,5,7
+export CUDA_VISIBLE_DEVICES=7
 
 # Run training
-torchrun --nproc_per_node=4 --master_port=$((12000 + RANDOM % 10000)) \
+torchrun --nproc_per_node=1 --master_port=$((12000 + RANDOM % 10000)) \
     -m scripts.train \
     --config=cosmos_predict2/_src/predict2/action/configs/action_conditioned/config.py \
-    -- experiment=ac_reason_embeddings_rectified_flow_2b_agibot_480_1920 \
+    -- experiment=ac_reason_embeddings_rectified_flow_2b_multiview_448_1344 \
     ~dataloader_train.dataloaders
 
 echo "Training completed!"
--- scripts/train/train_ac_mv_agibot.sh
+++ scripts/train/train_ac_mv_agibot.sh
@@ -8,13 +8,13 @@ set -e
 export IMAGINAIRE_OUTPUT_ROOT=/raid/chen.xin/repo/cosmos-predict2.5/imaginaire-output
 
 # Set visible GPU(s)
-export CUDA_VISIBLE_DEVICES=3,4
+export CUDA_VISIBLE_DEVICES=2
 
 # Run training
-torchrun --nproc_per_node=2 --master_port=$((12000 + RANDOM % 10000)) \
+torchrun --nproc_per_node=1 --master_port=$((12000 + RANDOM % 10000)) \
     -m scripts.train \
     --config=cosmos_predict2/_src/predict2/action/configs/action_conditioned/config.py \
-    -- experiment=ac_reason_embeddings_rectified_flow_2b_multiview_448_1344 \
+    -- experiment=ac_reason_embeddings_rectified_flow_2b_agibot_480_1920 \
     ~dataloader_train.dataloaders
 
 echo "Training completed!"
